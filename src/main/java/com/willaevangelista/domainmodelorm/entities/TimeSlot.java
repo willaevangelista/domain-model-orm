e1bd1790f5cfc531e60bca62de49c74561a07a79
willa <willaevangelista.pro@gmail.com>
2026-08-16 17:55:48 -0300

    fix: enforce TIMESTAMP WITHOUT TIME ZONE on TimeSlot start and end fields

--- src/main/java/com/willaevangelista/domainmodelorm/entities/TimeSlot.java
+++ src/main/java/com/willaevangelista/domainmodelorm/entities/TimeSlot.java
@@ -17,7 +17,11 @@ public class TimeSlot implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant inicio;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant fim;
 
     @ManyToOne
