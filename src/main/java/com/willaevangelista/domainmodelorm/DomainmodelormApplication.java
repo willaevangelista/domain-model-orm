8b549c12919daa0eea55d47e27070b12d44ab2b3
willa <willaevangelista.pro@gmail.com>
2026-08-16 16:31:13 -0300

    chore: initial project setup

---
+++ src/main/java/com/willaevangelista/domainmodelorm/DomainmodelormApplication.java
@@ -0,0 +1,13 @@
+package com.willaevangelista.domainmodelorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainmodelormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainmodelormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willaevangelista/domainmodelorm/DomainmodelormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willaevangelista.domainmodelorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainmodelormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
